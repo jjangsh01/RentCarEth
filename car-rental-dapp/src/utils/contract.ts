28c43e8a9a478cd042a760c47aae7b49021fd1f7
jjangsh01 <jjangsh01@gmail.com>
2025-05-30 16:44:03 +0900

    Deleted all files from mergetest branch

--- car-rental-dapp/src/utils/contract.ts
+++
@@ -1,8 +0,0 @@
-import { ethers } from "ethers";
-import abi from "../artifacts/CarRental.json";  // Compile and place ABI here
-
-const contractAddress = process.env.REACT_APP_CONTRACT_ADDRESS || "";
-
-export const getContract = (provider: ethers.providers.Web3Provider) => {
-  return new ethers.Contract(contractAddress, abi, provider.getSigner());
-};
